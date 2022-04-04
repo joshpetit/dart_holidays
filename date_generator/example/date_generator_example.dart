e8da30a1fa64e8385bed008eebb67a0454eedf94
Joshua Petitma <joshua@petit.dev>
2022-04-04 13:09:18 -0400

    Add date generator template package

---
+++ date_generator/example/date_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:date_generator/date_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ date_generator/lib/date_generator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library date_generator;
+
+export 'src/date_generator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
