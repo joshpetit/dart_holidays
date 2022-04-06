3816ff33c2f9abb848fdd589e0be6af20e58af0b
Joshua Petitma <joshua@petit.dev>
2022-04-06 07:08:56 -0400

    Add tests for month

--- packages/date_generator/test/date_generator_test.dart
+++ packages/date_generator/test/date_generator_test.dart
@@ -101,7 +101,7 @@ void main() {
     });
   });
 
-  group('Order creates the correct days', ([int ordinal = 1]) {
+  group('Order creates the correct days', () {
     Order order;
     Day day;
     test('That january created correctly', () {
@@ -153,5 +153,138 @@ void main() {
       expect(day.ordinal, equals(1));
       expect(day.day, equals(6));
     });
+  });
+
+  group('Months created correctly', () {
+    Order order;
+    Day day;
+    Month month;
+
+    test('should have month 0 for january', () {
+      order = gen.first();
+      day = order.sunday();
+      month = day.january();
+      expect(month.ordinal, equals(1));
+      expect(month.day, equals(0));
+      expect(month.month, equals(0));
+    });
+
+    test('should have month 1 for february', () {
+      order = gen.second();
+      day = order.monday();
+      month = day.february();
+      expect(month.ordinal, equals(2));
+      expect(month.day, equals(1));
+      expect(month.month, equals(1));
+    });
+
+    test('should have month 2 for march', () {
+      order = gen.third();
+      day = order.tuesday();
+      month = day.march();
+      expect(month.ordinal, equals(3));
+      expect(month.day, equals(2));
+      expect(month.month, equals(2));
+    });
+
+    test('should have month 3 for april', () {
+      order = gen.fourth();
+      day = order.wednesday();
+      month = day.april();
+      expect(month.ordinal, equals(4));
+      expect(month.day, equals(3));
+      expect(month.month, equals(3));
+    });
+
+    test('should have month 4 for may', () {
+      order = gen.last();
+      day = order.thursday();
+      month = day.may();
+      expect(month.ordinal, equals(5));
+      expect(month.day, equals(4));
+      expect(month.month, equals(4));
+    });
+
+    test('should have month 5 for june', () {
+      order = gen.first();
+      day = order.friday();
+      month = day.june();
+      expect(month.ordinal, equals(1));
+      expect(month.day, equals(5));
+      expect(month.month, equals(5));
+    });
+
+    test('should have month 6 for july', () {
+      order = gen.second();
+      day = order.saturday();
+      month = day.july();
+      expect(month.ordinal, equals(2));
+      expect(month.day, equals(6));
+      expect(month.month, equals(6));
+    });
+
+    test('should have month 7 for august', () {
+      order = gen.third();
+      day = order.sunday();
+      month = day.august();
+      expect(month.ordinal, equals(3));
+      expect(month.day, equals(0));
+      expect(month.month, equals(7));
+    });
+
+    test('should have month 8 for september', () {
+      order = gen.fourth();
+      day = order.monday();
+      month = day.september();
+      expect(month.ordinal, equals(4));
+      expect(month.day, equals(1));
+      expect(month.month, equals(8));
+    });
+
+    test('should have month 9 for october', () {
+      order = gen.last();
+      day = order.tuesday();
+      month = day.october();
+      expect(month.ordinal, equals(5));
+      expect(month.day, equals(2));
+      expect(month.month, equals(9));
+    });
+
+    test('should have month 10 for november', () {
+      order = gen.first();
+      day = order.wednesday();
+      month = day.november();
+      expect(month.ordinal, equals(1));
+      expect(month.day, equals(3));
+      expect(month.month, equals(10));
+    });
+
+    test('should have month 11 for december', () {
+      order = gen.second();
+      day = order.thursday();
+      month = day.december();
+      expect(month.ordinal, equals(2));
+      expect(month.day, equals(4));
+      expect(month.month, equals(11));
+    });
+
+    test('should have month 5 in(5)', () {
+      order = gen.second();
+      day = order.thursday();
+      month = day.inside(5);
+      expect(month.ordinal, equals(2));
+      expect(month.day, equals(4));
+      expect(month.month, equals(5));
+    });
+
+    test('should allow month(#)', () {
+      order = gen.second();
+      day = order.thursday();
+      month = day.month(5);
+      expect(month.ordinal, equals(2));
+      expect(month.day, equals(4));
+      expect(month.month, equals(5));
+    });
+
   });
 }
