3432c4ee77229a75a4d9b85d89455c8fa0aa896b
Joshua Petitma <joshua@petit.dev>
2022-04-06 06:28:27 -0400

    Update package name in export

--- packages/date_generator/lib/date_generator.dart
+++ packages/date_generator/lib/date_generator.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library date_generator;
 
-export 'src/date_generator_base.dart';
+export 'src/date_generator.dart';
 
 // TODO: Export any libraries intended for clients of this package.
