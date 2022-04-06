305cb72a801802ac33f6d2360a0dc2229f89085e
Joshua Petitma <joshua@petit.dev>
2022-04-06 06:08:33 -0400

    Create base package and add tests for odials

--- packages/date_generator/lib/src/date_generator.dart
+++ packages/date_generator/lib/src/date_generator.dart
@@ -1,6 +1,6 @@
 // TODO: Put public facing types in this file.
 
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
+class Generator {
   bool get isAwesome => true;
 }
--- packages/date_generator/test/date_generator_test.dart
+++ packages/date_generator/test/date_generator_test.dart
@@ -2,15 +2,27 @@ import 'package:date_generator/date_generator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('Oridials created correctly', () {
+    final gen = Generator();
 
-    setUp(() {
-      // Additional setup goes here.
+    test('First ordial correct', () {
+      expect(gen.first().ordinal, equals(1));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Second ordial correct', () {
+      expect(gen.second().ordinal, equals(2));
+    });
+
+    test('Third ordial correct', () {
+      expect(gen.third().ordinal, equals(3));
+    });
+
+    test('Fourth ordial correct', () {
+      expect(gen.fourth().ordinal, equals(4));
+    });
+
+    test('Last ordial correct', () {
+      expect(gen.last().ordinal, equals(5));
     });
   });
 }
