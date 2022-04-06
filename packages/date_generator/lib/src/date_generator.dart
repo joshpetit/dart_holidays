272576db9620b87508b4cf75eb99e716dcd2c59a
Joshua Petitma <joshua@petit.dev>
2022-04-06 06:38:57 -0400

    Implement day and ordinals

--- packages/date_generator/lib/src/date_generator.dart
+++ packages/date_generator/lib/src/date_generator.dart
@@ -1,6 +1,31 @@
-// TODO: Put public facing types in this file.
-
 /// Checks if you are awesome. Spoiler: you are.
 class Generator {
   bool get isAwesome => true;
+
+  Order first() => Order(1);
+  Order second() => Order(2);
+  Order third() => Order(3);
+  Order fourth() => Order(4);
+  Order last() => Order(5);
+
+  Day sunday([int ordinal = 1]) => Day(ordinal, 0);
+  Day monday([int ordinal = 1]) => Day(ordinal, 1);
+  Day tuesday([int ordinal = 1]) => Day(ordinal, 2);
+  Day wednesday([int ordinal = 1]) => Day(ordinal, 3);
+  Day thursday([int ordinal = 1]) => Day(ordinal, 4);
+  Day friday([int ordinal = 1]) => Day(ordinal, 5);
+  Day saturday([int ordinal = 1]) => Day(ordinal, 6);
+}
+
+class Order {
+  const Order(this.ordinal);
+
+  final int ordinal;
+}
+
+class Day {
+  const Day(this.ordinal, this.day);
+
+  final int ordinal;
+  final int day;
 }
