5c917a0dc34987a5a619d2c56e3a407328847c2c
Joshua Petitma <joshua@petit.dev>
2022-04-06 15:02:01 -0400

    Add example to package

--- packages/date_generator/example/date_generator_example.dart
+++ packages/date_generator/example/date_generator_example.dart
@@ -1,6 +1,23 @@
 import 'package:date_generator/date_generator.dart';
 
 void main() {
-  var awesome = Generator();
-  print('awesome: ${awesome.isAwesome}');
+  var gen = Generator();
+
+  var res = gen.first.sunday.january.of(2020);
+  print(res);
+  res = gen.second.thursday.march.of(1900);
+  print(res);
+
+  // Equivalents
+  res = gen.last.monday.december.of(2021);
+  var same = gen.week(5).monday.december.of(2021);
+  print(res.toIso8601String() == same.toIso8601String());
+
+  res = gen.last.monday.december.of(2021);
+  same = gen.last.monday.month(12).of(2021);
+  print(res.toIso8601String() == same.toIso8601String());
+
+  res = gen.last.monday.december.of(2021);
+  same = gen.week(5).monday.month(12).of(2021);
+  print(res.toIso8601String() == same.toIso8601String());
 }
