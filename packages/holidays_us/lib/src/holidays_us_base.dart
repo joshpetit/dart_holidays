d8ffab376ed517ccae4e1302e6ed439067c49fad
Joshua Petitma <joshua@petit.dev>
2022-04-08 08:57:52 -0400

    Add documentation

--- packages/holidays_us/lib/src/holidays_us_base.dart
+++ packages/holidays_us/lib/src/holidays_us_base.dart
@@ -1,6 +1,7 @@
 import 'package:date_generator/date_generator.dart';
 import 'package:date_generator/holiday_group.dart';
 
+/// Holidays within the United States
 class USHolidays extends HolidayGroup {
   USHolidays() {
     addHolidays({
@@ -21,22 +22,39 @@ class USHolidays extends HolidayGroup {
     });
   }
 
+  /// MLK Day is the third monday in January
   DateTime martinLutherKingDay(int year) =>
       Generator().third.monday.january.of(year);
+
+  /// President's is the third monday in February
   DateTime presidentsDay(int year) =>
       Generator().third.monday.february.of(year);
+
+  /// Memorial day is the last monday of May
   DateTime memorialDay(int year) => Generator().last.monday.may.of(year);
+
+  /// Mother's day is the second Sunday of May
   DateTime mothersDay(int year) => Generator().second.sunday.may.of(year);
+
+  /// Father's day is the third Sunday of June
   DateTime fathersDay(int year) => Generator().third.sunday.june.of(year);
+
+  /// Labor day is the first monday of September
   DateTime laborDay(int year) => Generator().first.monday.september.of(year);
+
+  /// Columbus day is the second monday of October
   DateTime columbusDay(int year) => Generator().second.monday.october.of(year);
   DateTime halloween(int year) => DateTime(year, 10, 31);
   DateTime newYearsDay(int year) => DateTime(year, 1, 1);
   DateTime christmas(int year) => DateTime(year, 12, 25);
   DateTime valentinesDay(int year) => DateTime(year, 2, 14);
   DateTime independenceDay(int year) => DateTime(year, 7, 4);
+
+  /// Thanks giving is the fourth thursday of November
   DateTime thanksgiving(int year) =>
       Generator().fourth.thursday.november.of(year);
+
+  /// It taks 10 mathmeticians and 100 prayers to calculate easter's date.
   DateTime easter(int year) {
     int Y = year;
     int a = Y % 19;
