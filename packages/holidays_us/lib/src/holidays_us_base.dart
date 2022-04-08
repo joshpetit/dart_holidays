4b1df09456dc18ec3a2ef010ca560a223c9dae6c
Joshua Petitma <joshua@petit.dev>
2022-04-08 08:48:31 -0400

    Add easter

--- packages/holidays_us/lib/src/holidays_us_base.dart
+++ packages/holidays_us/lib/src/holidays_us_base.dart
@@ -4,7 +4,7 @@ import 'package:date_generator/holiday_group.dart';
 class USHolidays extends HolidayGroup {
   USHolidays() {
     addHolidays({
-      /* 'easter': easter, */
+      'easter': easter,
       'thanksgiving': thanksgiving,
       'independenceDay': independenceDay,
       'valentinesDay': valentinesDay,
@@ -37,38 +37,23 @@ class USHolidays extends HolidayGroup {
   DateTime independenceDay(int year) => DateTime(year, 7, 4);
   DateTime thanksgiving(int year) =>
       Generator().fourth.thursday.november.of(year);
-  /* DateTime easter(int year) { */
-  /*   // implementation of anonymous gregorian algorithm */
-  /*   double L, a, b, c, d, day, e, f, g, h, i, k, m, month; */
-  /**/
-  /*   a = year % 19; */
-  /*   b = year / 100; */
-  /*   b.floor(); */
-  /**/
-  /*   c = year % 100; */
-  /*   d = b / 4; */
-  /*   d.floor(); */
-  /**/
-  /*   e = b % 4; */
-  /*   f = ((b + 8) / 25); */
-  /*   f.floor(); */
-  /**/
-  /*   g = ((b - f + 1) / 3); */
-  /*   g.floor(); */
-  /**/
-  /*   h = (19 * a + b - d - g + 15) % 30; */
-  /*   i = c / 4; */
-  /*   i.floor(); */
-  /*   k = c % 4; */
-  /*   L = (32 + 2 * e + 2 * i - h - k) % 7; */
-  /**/
-  /*   m = (a + 11 * h + 22 * L) / 451; */
-  /*   m.floor(); */
-  /*   month = ((h + L - 7 * m + 114) / 31) - 1; */
-  /*   month.floor(); */
-  /**/
-  /*   day = ((h + L - 7 * m + 114) % 31) + 1; */
-  /*   DateTime date = DateTime(year, month.toInt(), day.toInt()); */
-  /*   return date; */
-  /* } */
+  DateTime easter(int year) {
+    int Y = year;
+    int a = Y % 19;
+    int b = Y ~/ 100;
+    int c = Y % 100;
+    int d = b ~/ 4;
+    int e = b % 4;
+    int f = (b + 8) ~/ 25;
+    int g = (b - f + 1) ~/ 3;
+    int h = (19 * a + b - d - g + 15) % 30;
+    int i = c ~/ 4;
+    int k = c % 4;
+    int L = (32 + 2 * e + 2 * i - h - k) % 7;
+    int m = (a + 11 * h + 22 * L) ~/ 451;
+    int month = (h + L - 7 * m + 114) ~/ 31;
+    int day = ((h + L - 7 * m + 114) % 31) + 1;
+    DateTime dt = DateTime(year, month, day);
+    return dt;
+  }
 }
